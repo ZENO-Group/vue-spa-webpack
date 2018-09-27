08e189ec86e7b1abf041c3a44a97de3abd12ae83
Nikk Li <wyan_li@hnair.com>
2018-09-28 01:02:11 +0800

    fix ? expression bug

--- template/src/layouts/MainLayout/index.vue
+++ template/src/layouts/MainLayout/index.vue
@@ -11,7 +11,7 @@
         <div class="main-content">
           <el-breadcrumb class="no-select" v-if="breadcrumb" separator="/">
             <template v-for="route in currentRoute">
-              <el-breadcrumb-item :key="route.path" :to="{ path: route.path }">{{ route.meta ? route.meta.display: '' }}</el-breadcrumb-item>
+              <el-breadcrumb-item :key="route.path" :to="{ path: route.path }">{{ route.meta | showDisplay }}</el-breadcrumb-item>
             </template>
           </el-breadcrumb>
           <router-view></router-view>
@@ -33,6 +33,11 @@ export default {
   data () {
     return {
       routes: []
+    }
+  },
+  filters: {
+    showDisplay(meta) {
+      return meta.display
     }
   },
   computed: {
